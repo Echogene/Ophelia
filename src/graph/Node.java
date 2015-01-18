e688a5c9bf079005108868187f435847a1736c93
Echogene <echogene.alpha@gmail.com>
2015-01-18 15:19:06 +0000

    Add preliminary interfaces for defining graphs

---
+++ src/graph/Node.java
@@ -0,0 +1,9 @@
+package graph;
+
+/**
+ * A node of a graph.  This is just a tagging interface and doesn't define any methods to implement.
+ * @author Steven Weston
+ */
+public interface Node {
+
+}
---
+++ src/graph/edge/Edge.java
@@ -0,0 +1,14 @@
+package graph.edge;
+
+import graph.Node;
+
+import java.util.Collection;
+
+/**
+ * An edge connects a collection of nodes (the ends) together in a graph.
+ * @author Steven Weston
+ */
+public interface Edge<N extends Node, C extends Collection<N>> {
+
+	C getEnds();
+}
