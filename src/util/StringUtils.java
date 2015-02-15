ffc3adcfa68d7d1eb199940101a48ecd850f4d6b
Echogene <echogene.alpha@gmail.com>
2015-02-15 14:11:21 +0000

    Document and stuff

--- src/util/StringUtils.java
+++ src/util/StringUtils.java
@@ -1,11 +1,13 @@
 package util;
 
 import com.sun.istack.internal.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static util.NumberUtils.isOdd;
 
@@ -14,17 +16,24 @@ import static util.NumberUtils.isOdd;
  */
 public class StringUtils {
 
-	public static String addCharacterAfterEveryNewline(String lines, char character) {
+	@NotNull
+	public static String addCharacterAfterEveryNewline(
+			@NotNull String lines,
+			char character
+	) {
 		return character + lines.replace("\n", "\n" + character);
 	}
 
 	/**
-	 * Join a list of strings using another string.
-	 * @param list
-	 * @param joiner
-	 * @return
+	 * @param list a list of strings to concatenate
+	 * @param joiner the string to insert between consecutive pairs of strings
+	 * @return the concatenation of the list of strings with the given joiner between consecutive pairs
 	 */
-	public static String join(Collection<String> list, String joiner) {
+	@NotNull
+	public static String join(
+			@NotNull Collection<String> list,
+			@NotNull String joiner
+	) {
 		StringBuilder sb = new StringBuilder();
 		boolean first = true;
 		for (String string : list) {
@@ -38,16 +47,26 @@ public class StringUtils {
 	}
 
 	/**
-	 * Count the number of times a character occurs in a string.
-	 * @param character
+	 * @param substring
 	 * @param string
-	 * @return
+	 * @return the number of times the substring appears in the given string
 	 */
-	public static int count(String character, String string) {
-		return string.length() - string.replace(character, "").length();
+	public static int count(
+			@NotNull String substring,
+			@NotNull String string
+	) {
+		return string.length() - string.replace(substring, "").length();
 	}
 
-	public static String substring(String string, int startIndex, int endIndex) {
+	/**
+	 * @param string
+	 * @param startIndex
+	 * @param endIndex
+	 * @return the substring of the given string starting and ending at the given indices.  If the end index is -1, then
+	 *         it is treated as the index of the end of the string
+	 */
+	@NotNull
+	public static String substring(@NotNull String string, int startIndex, int endIndex) {
 		if (endIndex == -1) {
 			return string.substring(startIndex);
 		} else {
@@ -55,24 +74,37 @@ public class StringUtils {
 		}
 	}
 
+	/**
+	 * Print the concatenation of the strings obtained from applying the given presentation function to the given
+	 * collection.  Then print another line of the same length highlighting which elements match the given predicate.
+	 * @param collection
+	 * @param predicate
+	 * @param presentationFunction
+	 * @param <T>
+	 * @return
+	 */
+	@NotNull
 	public static <T> String identify(
-			Collection<T> collection,
-			Function<T, Boolean> identifier,
-			Function<T, String> presentationFunction
+			@NotNull Collection<T> collection,
+			@NotNull Predicate<T> predicate,
+			@NotNull Function<T, String> presentationFunction
 	) {
 
-		Map<T, String> map = new HashMap<>();
+		Map<T, String> presentations = new HashMap<>();
 		for (T t : collection) {
-			map.put(t, presentationFunction.apply(t));
+			presentations.put(t, presentationFunction.apply(t));
 		}
 		StringBuilder sb = new StringBuilder();
 		for (T t : collection) {
-			sb.append(map.get(t));
+			sb.append(presentations.get(t));
 		}
 		sb.append("\n");
 		for (T t : collection) {
-			int length = map.get(t).length();
-			if (identifier.apply(t)) {
+			int length = presentations.get(t).length();
+			if (length == 0) {
+				continue; // Even if the element matches, we can't really point it out.
+			}
+			if (predicate.test(t)) {
 				if (length == 1) {
 					sb.append("│");
 				} else if (length == 2) {
@@ -103,6 +135,10 @@ public class StringUtils {
 		}
 	}
 
+	/**
+	 * @return the string "null" if the input is null, otherwise the result of its toString
+	 */
+	@NotNull
 	public static String safeToString(@Nullable Object o) {
 		if (o == null) {
 			return "null";
