98ac06d9c6e4e561d66ed9bf54f48e58b1c4a939
Echogene <echogene.alpha@gmail.com>
2015-02-22 13:14:27 +0000

    Add some doc

--- src/exceptions/NotImplementedYetException.java
+++ src/exceptions/NotImplementedYetException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 /**
+ * Indicates that this function has not been implemented yet or is under construction.
  * @author Steven Weston
  */
 public class NotImplementedYetException extends RuntimeException {
