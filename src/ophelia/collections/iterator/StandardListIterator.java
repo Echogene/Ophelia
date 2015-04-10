b11728b0553c849d2147988f7757864e27a3c1ee
Echogene <echogene.alpha@gmail.com>
2015-04-11 00:54:36 +0100

    Add a standard list iterator to implement the split-up interfaces

---
+++ src/ophelia/collections/iterator/StandardListIterator.java
@@ -0,0 +1,60 @@
+package ophelia.collections.iterator;
+
+import java.util.ListIterator;
+
+/**
+ * @author Steven Weston
+ */
+public class StandardListIterator<E> implements ModifiableListIterator<E>, ListIterator<E> {
+
+	private final ListIterator<E> listIterator;
+
+	public StandardListIterator(ListIterator<E> listIterator) {
+		this.listIterator = listIterator;
+	}
+
+	@Override
+	public void remove() {
+		listIterator.remove();
+	}
+
+	@Override
+	public void set(E e) {
+		listIterator.set(e);
+	}
+
+	@Override
+	public void add(E e) {
+		listIterator.add(e);
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		return listIterator.hasPrevious();
+	}
+
+	@Override
+	public E previous() {
+		return listIterator.previous();
+	}
+
+	@Override
+	public int nextIndex() {
+		return listIterator.nextIndex();
+	}
+
+	@Override
+	public int previousIndex() {
+		return listIterator.previousIndex();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return listIterator.hasNext();
+	}
+
+	@Override
+	public E next() {
+		return listIterator.next();
+	}
+}
