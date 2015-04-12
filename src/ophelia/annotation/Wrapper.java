08421f6665da0f5f0045aecd7cd965b3c19a484d
Echogene <echogene.alpha@gmail.com>
2015-04-13 00:10:50 +0100

    Retain annotation for testing

--- src/ophelia/annotation/Wrapper.java
+++ src/ophelia/annotation/Wrapper.java
@@ -1,8 +1,12 @@
 package ophelia.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author Steven Weston
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Wrapper {
 
 	Class value();
