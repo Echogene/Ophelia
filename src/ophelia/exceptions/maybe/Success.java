4c68af25e3bed011dd6c6528e8c9d8161d371cda
Steven Weston <echogene.alpha@gmail.com>
2015-04-18 10:56:00 +0100

    Factor out failure handler

--- src/ophelia/exceptions/maybe/Success.java
+++ src/ophelia/exceptions/maybe/Success.java
@@ -1,6 +1,7 @@
 package ophelia.exceptions.maybe;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -38,63 +39,46 @@ class Success<R, E extends Exception> implements Maybe<R, E> {
 
 	@Override
 	public FailureHandler<R, E> returnOnSuccess() {
-		return new FailureHandler<R, E>() {
-			@Override
-			public R handleFailure(@NotNull Function<E, R> exceptionHandler) {
-				return value;
-			}
-
-			@Override
-			public R throwFailure() throws E {
-				return value;
-			}
-
-			@Override
-			public <F extends Exception> R throwMappedFailure(@NotNull Function<E, F> exceptionTransformer) throws F {
-				return value;
-			}
-
-			@NotNull
-			@Override
-			public R defaultOnFailure(@NotNull R defaultValue) {
-				return value;
-			}
-
-			@Override
-			public R nullOnFailure() {
-				return value;
-			}
-		};
+		return new DefaultFailureHandler<>(value);
 	}
 
 	@Override
 	public <S> FailureHandler<S, E> mapOnSuccess(Function<R, S> function) {
-		return new FailureHandler<S, E>() {
-			@Override
-			public S handleFailure(@NotNull Function<E, S> exceptionHandler) {
-				return function.apply(value);
-			}
+		return new DefaultFailureHandler<>(function.apply(value));
+	}
 
-			@Override
-			public S throwFailure() throws E {
-				return function.apply(value);
-			}
+	private class DefaultFailureHandler<T> implements FailureHandler<T, E> {
+		private final T t;
 
-			@Override
-			public <F extends Exception> S throwMappedFailure(@NotNull Function<E, F> exceptionTransformer) throws F {
-				return function.apply(value);
-			}
+		private DefaultFailureHandler(T t) {
+			this.t = t;
+		}
 
-			@NotNull
-			@Override
-			public S defaultOnFailure(@NotNull S defaultValue) {
-				return function.apply(value);
-			}
+		@Override
+		public T handleFailure(@NotNull Function exceptionHandler) {
+			return t;
+		}
 
-			@Override
-			public S nullOnFailure() {
-				return function.apply(value);
-			}
-		};
+		@Override
+		public T throwFailure() throws E {
+			return t;
+		}
+
+		@NotNull
+		@Override
+		public T defaultOnFailure(@NotNull Object defaultValue) {
+			return t;
+		}
+
+		@Nullable
+		@Override
+		public T nullOnFailure() {
+			return t;
+		}
+
+		@Override
+		public <F extends Exception> T throwMappedFailure(@NotNull Function<E, F> exceptionTransformer) throws F {
+			return t;
+		}
 	}
 }
