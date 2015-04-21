d84c4860d0f90544e35a2d18ab243b0eee75f038
Echogene <echogene.alpha@gmail.com>
2015-04-21 17:26:03 +0100

    Add documentation

--- src/ophelia/annotation/Wrapper.java
+++ src/ophelia/annotation/Wrapper.java
@@ -4,10 +4,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
+ * Indicates that the annotated class is a wrapper for another class.  A wrapper has a single field whose type is equal
+ * to the wrapped class.  It delegates all method calls to this field.
  * @author Steven Weston
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Wrapper {
 
+	/**
+	 * @return the class that is wrapped (the wrappee) by this wrapper
+	 */
 	Class<?> value();
 }
