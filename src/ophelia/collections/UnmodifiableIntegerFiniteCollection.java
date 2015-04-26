9e0e5f4d038a02e72c3f369b96a798fc14e330cd
Echogene <echogene.alpha@gmail.com>
2015-04-26 13:04:58 +0100

    Add an new unmodifiable wrapper

---
+++ src/ophelia/collections/UnmodifiableIntegerFiniteCollection.java
@@ -0,0 +1,76 @@
+package ophelia.collections;
+
+import ophelia.annotation.Wrapper;
+import ophelia.collections.iterator.UnmodifiableIterator;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+/**
+ * @author Steven Weston
+ */
+@Wrapper(Collection.class)
+public final class UnmodifiableIntegerFiniteCollection<E>
+		implements IntegerFiniteCollection<E, UnmodifiableIterator<E>> {
+
+	private final Collection<E> collection;
+
+	public UnmodifiableIntegerFiniteCollection(Collection<E> collection) {
+		this.collection = collection;
+	}
+
+	@Override
+	public int size() {
+		return collection.size();
+	}
+
+	@NotNull
+	@Override
+	public Object[] toArray() {
+		return collection.toArray();
+	}
+
+	@NotNull
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return collection.toArray(a);
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return collection.contains(o);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return collection.isEmpty();
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return collection.containsAll(c);
+	}
+
+	@Override
+	public Spliterator<E> spliterator() {
+		return collection.spliterator();
+	}
+
+	@Override
+	public Stream<E> stream() {
+		return collection.stream();
+	}
+
+	@Override
+	public Stream<E> parallelStream() {
+		return collection.parallelStream();
+	}
+
+	@NotNull
+	@Override
+	public UnmodifiableIterator<E> iterator() {
+		return new UnmodifiableIterator<>(collection.iterator());
+	}
+}
