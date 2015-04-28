3f0a4de6d6deec03db22981e4ed82144be0ea261
Echogene <echogene.alpha@gmail.com>
2015-04-28 20:48:26 +0100

    Add exceptional bifunction

---
+++ src/ophelia/util/function/ExceptionalBiFunction.java
@@ -0,0 +1,11 @@
+package ophelia.util.function;
+
+/**
+ * Like a {@link java.util.function.BiFunction}, but can throw an exception.
+ * @author Steven Weston
+ */
+@FunctionalInterface
+public interface ExceptionalBiFunction<D, F, R, E extends Exception> {
+
+	R apply(D d, F f) throws E;
+}
