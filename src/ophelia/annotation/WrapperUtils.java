7026b8204479c2867bc1b495030eaeabc05decee
Steven Weston <echogene.alpha@gmail.com>
2015-05-02 11:13:31 +0100

    Add some documentation

--- src/ophelia/annotation/WrapperUtils.java
+++ src/ophelia/annotation/WrapperUtils.java
@@ -8,15 +8,24 @@ import java.util.Set;
 import java.util.function.Function;
 
 /**
+ * A collection of methods to be used by classes annotated by {@link Wrapper}.
  * @author Steven Weston
  */
 public class WrapperUtils {
 
+	/**
+	 * Convert a set of type {@code V} to one of type {@code W} using the given function.  The given function should
+	 * be a constructor for a class {@code W} annotated with {@link Wrapper} for {@code V}.
+	 */
 	@NotNull
 	public static <V, W> Set<W> wrap(@NotNull Function<V, W> wrapper, @NotNull Set<V> wrappee) {
 		return FunctionUtils.image(wrappee, wrapper);
 	}
 
+	/**
+	 * Convert a list of type {@code V} to one of type {@code W} using the given function.  The given function should
+	 * be a constructor for a class {@code W} annotated with {@link Wrapper} for {@code V}.
+	 */
 	@NotNull
 	public static <V, W> List<W> wrap(@NotNull Function<V, W> wrapper, @NotNull List<V> wrappee) {
 		return FunctionUtils.image(wrappee, wrapper);
