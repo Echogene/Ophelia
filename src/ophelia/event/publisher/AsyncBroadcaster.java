b27a91e0a1f62a71c0091ccac97abe482aed847b
Echogene <echogene.alpha@gmail.com>
2015-05-08 21:42:29 +0100

    Document

--- src/ophelia/event/publisher/AsyncBroadcaster.java
+++ src/ophelia/event/publisher/AsyncBroadcaster.java
@@ -3,6 +3,7 @@ package ophelia.event.publisher;
 import ophelia.event.Event;
 
 /**
+ * A broadcaster that broadcasts to its subscribers asynchronously.
  * @author Steven Weston
  */
 public interface AsyncBroadcaster<E extends Event> extends Publisher<E> {
--- src/ophelia/event/publisher/SynchronousBroadcaster.java
+++ src/ophelia/event/publisher/SynchronousBroadcaster.java
@@ -3,6 +3,7 @@ package ophelia.event.publisher;
 import ophelia.event.Event;
 
 /**
+ * A broadcaster that broadcasts to its subscribers synchronously (though not necessarily in a dereministic order).
  * @author Steven Weston
  */
 public interface SynchronousBroadcaster<E extends Event> extends Publisher<E> {
