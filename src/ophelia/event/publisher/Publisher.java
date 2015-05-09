75b4b346b582868b8499b8c6a3712d63b08a84e0
Echogene <echogene.alpha@gmail.com>
2015-05-09 13:39:43 +0100

    Document

--- src/ophelia/event/publisher/Publisher.java
+++ src/ophelia/event/publisher/Publisher.java
@@ -5,6 +5,7 @@ import ophelia.event.Event;
 import java.util.function.Consumer;
 
 /**
+ * A publisher is an object that publishes events.  It can have subscribers to read the events when they are published.
  * @author Steven Weston
  */
 public interface Publisher<E extends Event> {
