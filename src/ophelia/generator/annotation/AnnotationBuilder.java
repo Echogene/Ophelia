e7831c37a8abd0f940fa39de011599050ffa2cb6
Echogene <echogene.alpha@gmail.com>
2015-05-14 17:31:36 +0100

    Add annotation builder

---
+++ src/ophelia/generator/annotation/AnnotationBuilder.java
@@ -0,0 +1,44 @@
+package ophelia.generator.annotation;
+
+import ophelia.collections.list.UnmodifiableList;
+import ophelia.generator.WithImportBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Steven Weston
+ */
+public class AnnotationBuilder implements WithImportBuilder<AnnotationBuilder> {
+
+	private final String annotationName;
+	private final List<String> imports = new ArrayList<>();
+
+	public AnnotationBuilder(Class<? extends Annotation> annotationClass) {
+		this.annotationName = annotationClass.getSimpleName();
+		this.withImport(annotationClass.getCanonicalName());
+	}
+
+	@Override
+	public AnnotationBuilder withImport(String classToImport) {
+		imports.add(classToImport);
+		return this;
+	}
+
+	public AnnotationWrapper build() {
+		return new AnnotationWrapper() {
+			@Override
+			public String getAnnotationName() {
+				return annotationName;
+			}
+
+			@NotNull
+			@Override
+			public UnmodifiableList<String> getImports() {
+				return new UnmodifiableList<>(imports);
+			}
+		};
+	}
+}
