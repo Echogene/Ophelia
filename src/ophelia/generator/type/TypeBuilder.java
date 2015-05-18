a9b348f3b1a709e3a3b3915f76b4aabe7e09e391
Echogene <echogene.alpha@gmail.com>
2015-05-18 23:46:40 +0100

    Create type builder

---
+++ src/ophelia/generator/type/TypeBuilder.java
@@ -0,0 +1,63 @@
+package ophelia.generator.type;
+
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+import ophelia.collections.set.UnmodifiableSet;
+import ophelia.generator.WithImportBuilder;
+import ophelia.generator.annotation.AnnotationWrapper;
+import ophelia.util.ClassUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Steven Weston
+ */
+public class TypeBuilder implements WithImportBuilder<TypeBuilder> {
+
+	private final String typeName;
+	private final Set<String> imports = new HashSet<>();
+	private final List<AnnotationExpr> annotations = new ArrayList<>();
+
+	public TypeBuilder(String canonicalClassName) {
+		this.typeName = ClassUtils.getSimpleName(canonicalClassName);
+		withImport(canonicalClassName);
+	}
+
+	public TypeBuilder(Class<?> clazz) {
+		this(clazz.getCanonicalName());
+	}
+
+	public TypeBuilder withAnnotation(final AnnotationWrapper annotation) {
+		annotations.add(annotation.getNode());
+		withImports(annotation.getImports().stream());
+		return this;
+	}
+
+	@Override
+	public TypeBuilder withImport(String canonicalClassName) {
+		imports.add(canonicalClassName);
+		return this;
+	}
+
+	public TypeWrapper build() {
+		return new TypeWrapper() {
+			@NotNull
+			@Override
+			public UnmodifiableSet<String> getImports() {
+				return new UnmodifiableSet<>(imports);
+			}
+
+			@Override
+			public Type getNode() {
+				ClassOrInterfaceType type = new ClassOrInterfaceType(typeName);
+				type.setAnnotations(annotations);
+				return type;
+			}
+		};
+	}
+}
---
+++ src/ophelia/generator/type/TypeWrapper.java
@@ -0,0 +1,11 @@
+package ophelia.generator.type;
+
+import com.github.javaparser.ast.type.Type;
+import ophelia.generator.CodeWrapperWithImports;
+
+/**
+ * @author Steven Weston
+ */
+public interface TypeWrapper extends CodeWrapperWithImports<Type> {
+
+}
