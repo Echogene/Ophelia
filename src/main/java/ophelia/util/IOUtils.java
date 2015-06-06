46b53a305ffa03c0304bc8df8e3b705b8e3af101
Echogene <echogene.alpha@gmail.com>
2015-06-06 09:47:16 +0100

    Add annotation

--- src/main/java/ophelia/util/IOUtils.java
+++ src/main/java/ophelia/util/IOUtils.java
@@ -1,7 +1,9 @@
 package ophelia.util;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.InputStream;
-import java.lang.String;import java.util.Scanner;
+import java.util.Scanner;
 
 /**
  * @author Steven Weston
@@ -10,10 +12,9 @@ public class IOUtils {
 
 	/**
 	 * Obtained from <a href=http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string>here</a>
-	 * @param is
-	 * @return
 	 */
-	public static String convertStreamToString(InputStream is) {
+	@NotNull
+	public static String convertStreamToString(@NotNull InputStream is) {
 		Scanner s = new Scanner(is).useDelimiter("\\A");
 		return s.hasNext() ? s.next() : "";
 	}
