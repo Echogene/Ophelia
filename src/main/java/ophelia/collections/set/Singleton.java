e36fe2d0996ef3282cdbafe375e488fdbf197b4d
Steven Weston <echogene.alpha@gmail.com>
2015-08-12 20:54:58 +0100

    Add singleton set

---
+++ src/main/java/ophelia/collections/set/Singleton.java
@@ -0,0 +1,81 @@
+package ophelia.collections.set;
+
+import ophelia.collections.iterator.UnmodifiableIterator;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static java.util.Collections.singleton;
+
+public final class Singleton<E> implements BaseSet<E, UnmodifiableIterator<E>> {
+
+	private final E element;
+	private final Set<E> set;
+
+	public Singleton(@NotNull E element) {
+		this.element = element;
+		set = singleton(element);
+	}
+
+	@Override
+	public int size() {
+		return 1;
+	}
+
+	@NotNull
+	@Override
+	public Object[] toArray() {
+		return new Object[] {element};
+	}
+
+	@NotNull
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return set.toArray(a);
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return element.equals(o);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return c.stream().allMatch(this::contains);
+	}
+
+	@NotNull
+	@Override
+	public UnmodifiableIterator<E> iterator() {
+		return new UnmodifiableIterator<>(set.iterator());
+	}
+
+	@Override
+	public void forEach(Consumer<? super E> consumer) {
+		consumer.accept(element);
+	}
+
+	@Override
+	public Spliterator<E> spliterator() {
+		return set.spliterator();
+	}
+
+	@Override
+	public Stream<E> stream() {
+		return set.stream();
+	}
+
+	@Override
+	public Stream<E> parallelStream() {
+		return set.parallelStream();
+	}
+}
