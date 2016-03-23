8188cd482880af065d404d28ccc5f78e2a9a2d30
Echogene <echogene.alpha@gmail.com>
2016-03-23 16:26:48 +0000

    Extract superclass

---
+++ src/main/java/ophelia/exceptions/CollectedException.java
@@ -0,0 +1,60 @@
+package ophelia.exceptions;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * @author Steven Weston
+ */
+public class CollectedException extends Exception {
+
+	protected final List<Exception> list;
+
+	public CollectedException(List<Exception> exceptions) {
+		list = exceptions;
+	}
+
+	@Override
+	public String getMessage() {
+
+		return list.stream()
+				.map(Exception::getMessage)
+				.reduce("", (s, t) -> s + "\n" + t);
+	}
+
+	@Override
+	public String getLocalizedMessage() {
+
+		return list.stream()
+				.map(Exception::getLocalizedMessage)
+				.reduce("", (s, t) -> s + "\n" + t);
+	}
+
+	@Override
+	public String toString() {
+
+		return list.stream()
+				.map(Exception::toString)
+				.reduce("", (s, t) -> s + "\n" + t);
+	}
+
+	@Override
+	public void printStackTrace() {
+		list.stream().forEach(Exception::printStackTrace);
+	}
+
+	@Override
+	public void printStackTrace(PrintWriter s) {
+		list.stream().forEach(e -> e.printStackTrace(s));
+	}
+
+	@Override
+	public void printStackTrace(PrintStream s) {
+		list.stream().forEach(e -> e.printStackTrace(s));
+	}
+
+	public List<Exception> getExceptions() {
+		return list;
+	}
+}
--- src/main/java/ophelia/exceptions/StackedException.java
+++ src/main/java/ophelia/exceptions/StackedException.java
@@ -1,66 +1,18 @@
 package ophelia.exceptions;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Steven Weston
  */
-public class StackedException extends Exception {
-
-	private final List<Exception> list;
+public class StackedException extends CollectedException {
 
 	public StackedException(Exception e) {
-		list = new ArrayList<>();
+		super(new ArrayList<>());
 		addException(e);
 	}
 
 	public void addException(Exception e) {
 		list.add(e);
-	}
-
-	@Override
-	public String getMessage() {
-
-		return list.stream()
-				.map(Exception::getMessage)
-				.reduce("", (s, t) -> s + "\n" + t);
-	}
-
-	@Override
-	public String getLocalizedMessage() {
-
-		return list.stream()
-				.map(Exception::getLocalizedMessage)
-				.reduce("", (s, t) -> s + "\n" + t);
-	}
-
-	@Override
-	public String toString() {
-
-		return list.stream()
-				.map(Exception::toString)
-				.reduce("", (s, t) -> s + "\n" + t);
-	}
-
-	@Override
-	public void printStackTrace() {
-		list.stream().forEach(Exception::printStackTrace);
-	}
-
-	@Override
-	public void printStackTrace(PrintWriter s) {
-		list.stream().forEach(e -> e.printStackTrace(s));
-	}
-
-	@Override
-	public void printStackTrace(PrintStream s) {
-		list.stream().forEach(e -> e.printStackTrace(s));
-	}
-
-	public List<Exception> getExceptions() {
-		return list;
 	}
 }
