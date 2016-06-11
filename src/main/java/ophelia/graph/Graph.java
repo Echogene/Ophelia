45c5c25c6a3920b0c9cf15defcbc2237e8ac152f
Steven Weston <echogene.alpha@gmail.com>
2016-06-11 11:21:48 +0100

    Rename methods

--- src/main/java/ophelia/graph/Graph.java
+++ src/main/java/ophelia/graph/Graph.java
@@ -12,10 +12,10 @@ public interface Graph<N, E extends BaseCollection<N>> {
 	/**
 	 * @return All the edges in the graph.
 	 */
-	BaseCollection<E> getAllEdges();
+	BaseCollection<E> getEdges();
 
 	/**
 	 * @return All of the nodes in the graph.
 	 */
-	BaseCollection<N> getAllNodes();
+	BaseCollection<N> getNodes();
 }
