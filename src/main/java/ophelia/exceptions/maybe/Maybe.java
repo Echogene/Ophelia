db54cccb8ae3c2fd91bbf40f50d22e7b2aa521b2
Steven Weston <echogene.alpha@gmail.com>
2016-08-06 00:41:47 +0100

    Add some more wrappers

--- src/main/java/ophelia/exceptions/maybe/Maybe.java
+++ src/main/java/ophelia/exceptions/maybe/Maybe.java
@@ -2,6 +2,7 @@ package ophelia.exceptions.maybe;
 
 import ophelia.exceptions.AmbiguityException;
 import ophelia.exceptions.CollectedException;
+import ophelia.function.ExceptionalBiFunction;
 import ophelia.function.ExceptionalFunction;
 import ophelia.function.ExceptionalSupplier;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +11,9 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Like {@link Optional}, but stores the exception in case of failure for future processing.
@@ -25,6 +28,22 @@ import java.util.function.Function;
 public interface Maybe<D> extends SuccessHandler<D> {
 
 	/**
+	 * Convert an exceptional supplier into a supplier of maybes.
+	 */
+	@NotNull
+	static <T> Supplier<Maybe<T>> wrap(
+			@NotNull ExceptionalSupplier<? extends T, ? extends Exception> supplier
+	) {
+		return () -> {
+			try {
+				return new Success<>(supplier.get());
+			} catch (Exception e) {
+				return new Failure<>(e);
+			}
+		};
+	}
+
+	/**
 	 * Convert an exceptional function into a function of maybes.
 	 */
 	@NotNull
@@ -34,6 +53,22 @@ public interface Maybe<D> extends SuccessHandler<D> {
 		return d -> {
 			try {
 				return new Success<>(function.apply(d));
+			} catch (Exception e) {
+				return new Failure<>(e);
+			}
+		};
+	}
+
+	/**
+	 * Convert an exceptional bifunction into a bifunction of maybes.
+	 */
+	@NotNull
+	static <D, F, R> BiFunction<D, F, Maybe<R>> wrap(
+			@NotNull ExceptionalBiFunction<D, F, R, ? extends Exception> function
+	) {
+		return (d, f) -> {
+			try {
+				return new Success<>(function.apply(d, f));
 			} catch (Exception e) {
 				return new Failure<>(e);
 			}
