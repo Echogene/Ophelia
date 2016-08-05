cdb8a5427d66cef956736d1a9a5ed327080ff598
Steven Weston <echogene.alpha@gmail.com>
2016-08-05 19:17:27 +0100

    Add potential messages for ambiguity exceptions

--- src/main/java/ophelia/exceptions/AmbiguityException.java
+++ src/main/java/ophelia/exceptions/AmbiguityException.java
@@ -1,9 +1,36 @@
 package ophelia.exceptions;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * Indicates that a Collection was expected to have exactly one member, but didn't.
  * @author Steven Weston
  */
 public class AmbiguityException extends Exception {
 
+	public AmbiguityException() {
+		super();
+	}
+
+	public <T> AmbiguityException(
+			@NotNull String message,
+			@NotNull Collection<T> ambiguousCollection,
+			@NotNull Function<? super T, String> presenter
+	) {
+		super(MessageFormat.format(
+				message,
+				ambiguousCollection.stream()
+						.map(presenter)
+						.collect(Collectors.joining(", ", "[", "]"))
+		));
+	}
+
+	public AmbiguityException(@NotNull String message, @NotNull Collection<?> ambiguousCollection) {
+		this(message, ambiguousCollection, Object::toString);
+	}
 }
