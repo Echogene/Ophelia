9f2f524569ed208975c100f9016bbeebeabfcd3d
Steven Weston <echogene.alpha@gmail.com>
2017-01-01 23:10:51 +0000

    Doc

--- src/main/java/ophelia/builder/SelfReturner.java
+++ src/main/java/ophelia/builder/SelfReturner.java
@@ -3,9 +3,14 @@ package ophelia.builder;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Represents something that can return itself.  Used by Builders.
  * @author Steven Weston
  */
 public interface SelfReturner<B> {
 
+	/**
+	 * Do nothing other than return this.
+	 * @return this
+	 */
 	@NotNull B noöp();
 }
