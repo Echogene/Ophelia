39a669b570d083af4eeaa9732408957e4e1d5145
Steven Weston <echogene.alpha@gmail.com>
2017-01-02 20:49:06 +0000

    Doc

--- src/main/java/ophelia/builder/Builder.java
+++ src/main/java/ophelia/builder/Builder.java
@@ -2,6 +2,11 @@ package ophelia.builder;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A builder can return itself and build a new object.
+ * @param <T> the type of thing built by this Builder
+ * @param <B> the type of the class implementing this interface
+ */
 public interface Builder<T, B extends Builder<T, B>> extends SelfReturner<B> {
 
 	@NotNull T build();
