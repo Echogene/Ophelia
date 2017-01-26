dda7568bf0a0964eb06c4eaa58a97b7b2ea48b7d
Steven Weston <echogene.alpha@gmail.com>
2017-01-26 21:59:09 +0000

    Revert accidental commit

--- src/main/java/ophelia/collections/pair/OrderedPair.java
+++ src/main/java/ophelia/collections/pair/OrderedPair.java
@@ -3,7 +3,6 @@ package ophelia.collections.pair;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * todo: the hash code and equals need overriding so that they aren't symmetrical
  * @author Steven Weston
  */
 public class OrderedPair<E> extends AbstractPair<E> {
