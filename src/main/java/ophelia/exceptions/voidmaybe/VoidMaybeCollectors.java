edb6940278e6a52231ebd682075ae5004e8ef7a5
Steven Weston <echogene.alpha@gmail.com>
2017-02-15 20:10:16 +0000

    Add more collectors and tests

--- src/main/java/ophelia/exceptions/voidmaybe/VoidMaybeCollectors.java
+++ src/main/java/ophelia/exceptions/voidmaybe/VoidMaybeCollectors.java
@@ -1,62 +1,124 @@
 package ophelia.exceptions.voidmaybe;
 
 import ophelia.collections.list.UnmodifiableList;
+import ophelia.exceptions.AmbiguityException;
 import ophelia.exceptions.CollectedException;
+import ophelia.util.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.MessageFormat;
 import java.util.*;
 import java.util.stream.Collector;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toList;
 import static ophelia.exceptions.voidmaybe.Success.SUCCESS;
+import static ophelia.util.MapUtils.updateListBasedMap;
 import static ophelia.util.MapUtils.updateSetBasedMap;
 
-public interface VoidMaybeCollectors {
+public class VoidMaybeCollectors {
+
+	/**
+	 * @return a collector that collects to a successful {@link VoidMaybe} when exactly one of its collectees is
+	 * successful, or to a failure otherwise.
+	 */
+	@NotNull
+	public static Collector<VoidMaybe, Map<Boolean, List<VoidMaybe>>, VoidMaybe> successOnExactlyOneSuccess() {
+		return Collector.of(
+				HashMap::new,
+				(map, m) -> updateListBasedMap(map, m.isSuccess(), m),
+				CollectionUtils::mergeToLeft,
+				map -> {
+					List<VoidMaybe> successes = map.getOrDefault(true, emptyList());
+					List<VoidMaybe> failures = map.getOrDefault(false, emptyList());
+					if (successes.isEmpty()) {
+						return collapseFailures(failures);
+
+					} else if (successes.size() == 1) {
+						return SUCCESS;
+
+					} else {
+						return VoidMaybe.failure(new AmbiguityException(MessageFormat.format(
+								"Expected exactly 1 success, but got {0}.",
+								successes.size()
+						)));
+					}
+				}
+		);
+	}
 
 	@NotNull
-	static Collector<VoidMaybe, Map<Boolean, Set<VoidMaybe>>, VoidMaybe> findAtLeastOneSuccess() {
+	private static VoidMaybe collapseFailures(@NotNull Collection<VoidMaybe> failures) {
+		if (failures.isEmpty()) {
+			return VoidMaybe.failure(new NoSuchElementException());
+
+		} else if (failures.size() > 1) {
+			return VoidMaybe.multipleFailures(
+					failures.stream()
+							.map(VoidMaybeHandler::getException)
+							.map(Optional::get)
+							.map(CollectedException::flatten)
+							.flatMap(UnmodifiableList::stream)
+							.collect(toList())
+			);
+		} else {
+			return failures.iterator().next();
+		}
+	}
+
+	/**
+	 * @return a collector that collects to a successful {@link VoidMaybe} when at least one of its collectees is
+	 * successful, or to a failure if there are no successes.
+	 */
+	@NotNull
+	public static Collector<VoidMaybe, Map<Boolean, Set<VoidMaybe>>, VoidMaybe> successOnAtLeastOneSuccess() {
 		return Collector.of(
 				HashMap::new,
 				(map, m) -> updateSetBasedMap(map, m.isSuccess(), m),
-				(left, right) -> {
-					left.putAll(right);
-					return left;
-				},
+				CollectionUtils::mergeToLeft,
 				map -> {
 					Set<VoidMaybe> successes = map.getOrDefault(true, emptySet());
 					Set<VoidMaybe> failures = map.getOrDefault(false, emptySet());
 					if (successes.isEmpty()) {
-						if (failures.isEmpty()) {
-							return VoidMaybe.failure(new NoSuchElementException());
-
-						} else if (failures.size() > 1) {
-							return VoidMaybe.multipleFailures(
-									failures.stream()
-											.map(VoidMaybeHandler::getException)
-											.map(Optional::get)
-											.map(CollectedException::flatten)
-											.flatMap(UnmodifiableList::stream)
-											.collect(toList())
-							);
-						} else {
-							return failures.iterator().next();
-						}
+						return collapseFailures(failures);
 					}
 					return SUCCESS;
 				}
 		);
 	}
 
+	/**
+	 * @return a collector that collects to a successful {@link VoidMaybe} when all of its collectees are successful,
+	 * or to a failure if any of them are failures
+	 */
 	@NotNull
-	static Collector<VoidMaybe, Map<Boolean, Set<VoidMaybe>>, VoidMaybe> merge() {
+	public static Collector<VoidMaybe, Map<Boolean, Set<VoidMaybe>>, VoidMaybe> successOnAllSuccess() {
 		return Collector.of(
 				HashMap::new,
 				(map, m) -> updateSetBasedMap(map, m.isSuccess(), m),
-				(left, right) -> {
-					left.putAll(right);
-					return left;
-				},
+				CollectionUtils::mergeToLeft,
+				map -> {
+					Set<VoidMaybe> failures = map.getOrDefault(false, emptySet());
+					if (failures.isEmpty()) {
+						return SUCCESS;
+					} else {
+						return collapseFailures(failures);
+					}
+				}
+		);
+	}
+
+	/**
+	 * @return a collector that collects to a successful {@link VoidMaybe} when all (and at least one) of its
+	 * collectees are successful, or to a failure if any of them are failures
+	 */
+	@NotNull
+	public static Collector<VoidMaybe, Map<Boolean, Set<VoidMaybe>>, VoidMaybe> merge() {
+		return Collector.of(
+				HashMap::new,
+				(map, m) -> updateSetBasedMap(map, m.isSuccess(), m),
+				CollectionUtils::mergeToLeft,
 				map -> {
 					Set<VoidMaybe> successes = map.getOrDefault(true, emptySet());
 					Set<VoidMaybe> failures = map.getOrDefault(false, emptySet());
@@ -66,17 +128,8 @@ public interface VoidMaybeCollectors {
 						} else {
 							return SUCCESS;
 						}
-					} else if (failures.size() > 1) {
-						return VoidMaybe.multipleFailures(
-								failures.stream()
-										.map(VoidMaybeHandler::getException)
-										.map(Optional::get)
-										.map(CollectedException::flatten)
-										.flatMap(UnmodifiableList::stream)
-										.collect(toList())
-						);
 					} else {
-						return failures.iterator().next();
+						return collapseFailures(failures);
 					}
 				}
 		);
--- src/test/java/ophelia/exceptions/voidmaybe/VoidMaybeCollectorsTest.java
+++ src/test/java/ophelia/exceptions/voidmaybe/VoidMaybeCollectorsTest.java
@@ -1,5 +1,6 @@
 package ophelia.exceptions.voidmaybe;
 
+import ophelia.exceptions.AmbiguityException;
 import ophelia.exceptions.maybe.MaybeCollectorsTest;
 import org.junit.Test;
 
@@ -17,26 +18,26 @@ import static org.hamcrest.core.Is.is;
 public class VoidMaybeCollectorsTest {
 
 	@Test
-	public void empty_set_should_not_be_unique() throws Exception {
-		VoidMaybe unique = Stream.<VoidMaybe>empty().collect(findAtLeastOneSuccess());
+	public void empty_set_should_not_be_success_for_at_least_one_success() throws Exception {
+		VoidMaybe unique = Stream.<VoidMaybe>empty().collect(successOnAtLeastOneSuccess());
 
 		assertThat(unique, failure(hasException(is(instanceOf(NoSuchElementException.class)))));
 	}
 
 	@Test
-	public void single_failure_should_not_be_unique() throws Exception {
+	public void single_failure_should_not_be_success_for_at_least_one_success() throws Exception {
 		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
-		VoidMaybe unique = Stream.of(failure).collect(findAtLeastOneSuccess());
+		VoidMaybe unique = Stream.of(failure).collect(successOnAtLeastOneSuccess());
 
 		assertThat(unique, is(failure));
 	}
 
 	@Test
-	public void multiple_failures_should_not_be_unique() throws Exception {
+	public void multiple_failures_should_not_be_success_for_at_least_one_success() throws Exception {
 		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
 		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
 
-		VoidMaybe unique = Stream.of(failure, failure2).collect(findAtLeastOneSuccess());
+		VoidMaybe unique = Stream.of(failure, failure2).collect(successOnAtLeastOneSuccess());
 
 		assertThat(unique, failure(hasException(hasException(is(instanceOf(NullPointerException.class))))));
 		assertThat(unique, failure(hasException(hasException(is(instanceOf(IllegalArgumentException.class))))));
@@ -46,31 +47,144 @@ public class VoidMaybeCollectorsTest {
 	 * @see MaybeCollectorsTest#multiple_successes_for_the_same_value_should_be_unique
 	 */
 	@Test
-	public void multiple_successes_should_be_unique() throws Exception {
+	public void multiple_successes_should_be_success_for_at_least_one_success() throws Exception {
 		VoidMaybe success = VoidMaybe.success();
 		VoidMaybe success2 = VoidMaybe.success();
 
-		VoidMaybe unique = Stream.of(success, success2).collect(findAtLeastOneSuccess());
+		VoidMaybe unique = Stream.of(success, success2).collect(successOnAtLeastOneSuccess());
 
 		assertThat(unique, success());
 	}
 
 	@Test
-	public void unique_success_should_be_unique() throws Exception {
+	public void unique_success_should_be_success_for_at_least_one_success() throws Exception {
 		VoidMaybe success = VoidMaybe.success();
 
-		VoidMaybe unique = Stream.of(success).collect(findAtLeastOneSuccess());
+		VoidMaybe unique = Stream.of(success).collect(successOnAtLeastOneSuccess());
 
 		assertThat(unique, success());
 	}
 
 	@Test
-	public void unique_success_with_some_failures_should_be_unique() throws Exception {
+	public void unique_success_with_some_failures_should_be_success_for_at_least_one_success() throws Exception {
 		VoidMaybe success = VoidMaybe.success();
 		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
 		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
 
-		VoidMaybe unique = Stream.of(success, failure, failure2).collect(findAtLeastOneSuccess());
+		VoidMaybe unique = Stream.of(success, failure, failure2).collect(successOnAtLeastOneSuccess());
+
+		assertThat(unique, success());
+	}
+
+	@Test
+	public void empty_set_should_not_be_success_for_all_success() throws Exception {
+		VoidMaybe unique = Stream.<VoidMaybe>empty().collect(successOnAllSuccess());
+
+		assertThat(unique, success());
+	}
+
+	@Test
+	public void single_failure_should_not_be_success_for_all_success() throws Exception {
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe unique = Stream.of(failure).collect(successOnAllSuccess());
+
+		assertThat(unique, is(failure));
+	}
+
+	@Test
+	public void multiple_failures_should_not_be_success_for_all_success() throws Exception {
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
+
+		VoidMaybe unique = Stream.of(failure, failure2).collect(successOnAllSuccess());
+
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(NullPointerException.class))))));
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(IllegalArgumentException.class))))));
+	}
+
+	@Test
+	public void multiple_successes_should_be_success_for_all_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+		VoidMaybe success2 = VoidMaybe.success();
+
+		VoidMaybe unique = Stream.of(success, success2).collect(successOnAllSuccess());
+
+		assertThat(unique, success());
+	}
+
+	@Test
+	public void unique_success_should_be_success_for_all_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+
+		VoidMaybe unique = Stream.of(success).collect(successOnAllSuccess());
+
+		assertThat(unique, success());
+	}
+
+	@Test
+	public void unique_success_with_some_failures_should_not_be_success_for_all_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
+
+		VoidMaybe unique = Stream.of(success, failure, failure2).collect(successOnAllSuccess());
+
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(NullPointerException.class))))));
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(IllegalArgumentException.class))))));
+	}
+
+	@Test
+	public void empty_set_should_not_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe unique = Stream.<VoidMaybe>empty().collect(successOnExactlyOneSuccess());
+
+		assertThat(unique, failure(hasException(is(instanceOf(NoSuchElementException.class)))));
+	}
+
+	@Test
+	public void single_failure_should_not_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe unique = Stream.of(failure).collect(successOnExactlyOneSuccess());
+
+		assertThat(unique, is(failure));
+	}
+
+	@Test
+	public void multiple_failures_should_not_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
+
+		VoidMaybe unique = Stream.of(failure, failure2).collect(successOnExactlyOneSuccess());
+
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(NullPointerException.class))))));
+		assertThat(unique, failure(hasException(hasException(is(instanceOf(IllegalArgumentException.class))))));
+	}
+
+	@Test
+	public void multiple_successes_should_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+		VoidMaybe success2 = VoidMaybe.success();
+
+		VoidMaybe unique = Stream.of(success, success2).collect(successOnExactlyOneSuccess());
+
+		assertThat(unique, failure(hasException(is(instanceOf(AmbiguityException.class)))));
+	}
+
+	@Test
+	public void unique_success_should_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+
+		VoidMaybe unique = Stream.of(success).collect(successOnExactlyOneSuccess());
+
+		assertThat(unique, success());
+	}
+
+	@Test
+	public void unique_success_with_some_failures_should_not_be_success_for_exactly_one_success() throws Exception {
+		VoidMaybe success = VoidMaybe.success();
+		VoidMaybe failure = VoidMaybe.failure(new NullPointerException());
+		VoidMaybe failure2 = VoidMaybe.failure(new IllegalArgumentException());
+
+		VoidMaybe unique = Stream.of(success, failure, failure2).collect(successOnExactlyOneSuccess());
 
 		assertThat(unique, success());
 	}
