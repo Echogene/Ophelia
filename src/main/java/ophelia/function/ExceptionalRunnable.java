fecd076a32664efa17ebecd28bd57dcf7ca04b45
Steven Weston <echogene.alpha@gmail.com>
2017-03-01 20:52:34 +0000

    Add some default methods for finally

--- src/main/java/ophelia/function/ExceptionalRunnable.java
+++ src/main/java/ophelia/function/ExceptionalRunnable.java
@@ -1,5 +1,7 @@
 package ophelia.function;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Like a {@link Runnable} but can throw an Exception
  * @author Steven Weston
@@ -8,4 +10,32 @@ package ophelia.function;
 public interface ExceptionalRunnable<E extends Exception> {
 
 	void run() throws E;
+
+	/**
+	 * Try to run this runnable and always run the given runnable whether or not this one fails.
+	 */
+	@NotNull
+	default ExceptionalRunnable<E> andFinally(@NotNull Runnable r) {
+		return () -> {
+			try {
+				run();
+			} finally {
+				r.run();
+			}
+		};
+	}
+
+	/**
+	 * Try to run this runnable and always try to run the given runnable whether or not this one fails.
+	 */
+	@NotNull
+	default ExceptionalRunnable<E> andFinally(@NotNull ExceptionalRunnable<E> r) {
+		return () -> {
+			try {
+				run();
+			} finally {
+				r.run();
+			}
+		};
+	}
 }
