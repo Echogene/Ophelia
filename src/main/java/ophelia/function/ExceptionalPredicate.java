9dc0dba3e8c2330d62a3adfea0f92d05222645ab
Steven Weston <echogene.alpha@gmail.com>
2017-03-01 21:21:08 +0000

    Add some default methods

--- src/main/java/ophelia/function/ExceptionalPredicate.java
+++ src/main/java/ophelia/function/ExceptionalPredicate.java
@@ -1,5 +1,7 @@
 package ophelia.function;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Predicate;
 
 /**
@@ -8,4 +10,29 @@ import java.util.function.Predicate;
 @FunctionalInterface
 public interface ExceptionalPredicate<T, E extends Exception> {
 	boolean test(T t) throws E;
+
+	@NotNull
+	default ExceptionalPredicate<T, E> and(@NotNull Predicate<? super T> other) {
+		return t -> test(t) && other.test(t);
+	}
+
+	@NotNull
+	default ExceptionalPredicate<T, E> and(@NotNull ExceptionalPredicate<? super T, ? extends E> other) {
+		return t -> test(t) && other.test(t);
+	}
+
+	@NotNull
+	default ExceptionalPredicate<T, E> negate() {
+		return t -> !test(t);
+	}
+
+	@NotNull
+	default ExceptionalPredicate<T, E> or(@NotNull Predicate<? super T> other) {
+		return t -> test(t) || other.test(t);
+	}
+
+	@NotNull
+	default ExceptionalPredicate<T, E> or(@NotNull ExceptionalPredicate<? super T, ? extends E> other) {
+		return t -> test(t) || other.test(t);
+	}
 }
