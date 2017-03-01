9876cb407725f66069645078e944f3d874cfff62
Steven Weston <echogene.alpha@gmail.com>
2017-03-01 20:55:23 +0000

    The currying methods should be static
    make this an interface

--- src/main/java/ophelia/util/function/FunctionUtils.java
+++ src/main/java/ophelia/util/function/FunctionUtils.java
@@ -15,13 +15,13 @@ import java.util.stream.Collectors;
 /**
  * @author Steven Weston
  */
-public class FunctionUtils {
+public interface FunctionUtils {
 
 	/**
 	 * Safely get something from a potentially null object, returning null if that object is null.
 	 */
 	@Contract("null,_ -> null")
-	public static <S, T> T safeGet(@Nullable S s, @NotNull Function<S, T> getter) {
+	static <S, T> T safeGet(@Nullable S s, @NotNull Function<S, T> getter) {
 		if (s == null) {
 			return null;
 		} else {
@@ -30,12 +30,12 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <S, T> Function<S, T> safeWrap(Function<S, T> getter) {
+	static <S, T> Function<S, T> safeWrap(Function<S, T> getter) {
 		return s -> s == null ? null : getter.apply(s);
 	}
 
 	@NotNull
-	public static <T> UnaryOperator<T> safeWrap(UnaryOperator<T> getter) {
+	static <T> UnaryOperator<T> safeWrap(UnaryOperator<T> getter) {
 		return s -> s == null ? null : getter.apply(s);
 	}
 
@@ -43,7 +43,7 @@ public class FunctionUtils {
 	 * @return the image (as a list) of the function on the given array.
 	 */
 	@NotNull
-	public static <S, T> List<T> image(@NotNull S[] list, @NotNull Function<? super S, T> function) {
+	static <S, T> List<T> image(@NotNull S[] list, @NotNull Function<? super S, T> function) {
 		return image(Arrays.asList(list), function);
 	}
 
@@ -51,7 +51,7 @@ public class FunctionUtils {
 	 * @return the image of the function on the given list.
 	 */
 	@NotNull
-	public static <S, T> List<T> image(@NotNull List<S> list, @NotNull Function<? super S, T> function) {
+	static <S, T> List<T> image(@NotNull List<S> list, @NotNull Function<? super S, T> function) {
 		return list.stream()
 				.map(function)
 				.collect(Collectors.toList());
@@ -61,14 +61,14 @@ public class FunctionUtils {
 	 * @return the image of the function on the given set.
 	 */
 	@NotNull
-	public static <S, T> Set<T> image(@NotNull Set<S> set, @NotNull Function<? super S, T> function) {
+	static <S, T> Set<T> image(@NotNull Set<S> set, @NotNull Function<? super S, T> function) {
 		return set.stream()
 				.map(function)
 				.collect(Collectors.toSet());
 	}
 
 	@Nullable
-	public static <S, T> T ignoreExceptions(@Nullable S s, @NotNull Function<S, T> function) {
+	static <S, T> T ignoreExceptions(@Nullable S s, @NotNull Function<S, T> function) {
 		try {
 			return function.apply(s);
 		} catch (Exception e) {
@@ -77,7 +77,7 @@ public class FunctionUtils {
 	}
 
 	@Nullable
-	public static <T, E extends Exception> T ignoreExceptions(@NotNull ExceptionalSupplier<T, E> supplier) {
+	static <T, E extends Exception> T ignoreExceptions(@NotNull ExceptionalSupplier<T, E> supplier) {
 		try {
 			return supplier.get();
 		} catch (Exception e) {
@@ -86,7 +86,7 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <T> Consumer<T> checkThenMaybeConsume(@NotNull Predicate<T> predicate, @NotNull Consumer<T> consumer) {
+	static <T> Consumer<T> checkThenMaybeConsume(@NotNull Predicate<T> predicate, @NotNull Consumer<T> consumer) {
 		return t -> {
 			if (predicate.test(t)) {
 				consumer.accept(t);
@@ -95,7 +95,10 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <S, T> BiConsumer<S, T> checkThenMaybeConsume(@NotNull BiPredicate<S, T> predicate, @NotNull BiConsumer<S, T> consumer) {
+	static <S, T> BiConsumer<S, T> checkThenMaybeConsume(
+			@NotNull BiPredicate<S, T> predicate,
+			@NotNull BiConsumer<S, T> consumer
+	) {
 		return (s, t) -> {
 			if (predicate.test(s, t)) {
 				consumer.accept(s, t);
@@ -104,7 +107,10 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <S, T> BiConsumer<S, T> checkFirstThenMaybeConsume(@NotNull Predicate<S> predicate, @NotNull BiConsumer<S, T> consumer) {
+	static <S, T> BiConsumer<S, T> checkFirstThenMaybeConsume(
+			@NotNull Predicate<S> predicate,
+			@NotNull BiConsumer<S, T> consumer
+	) {
 		return (s, t) -> {
 			if (predicate.test(s)) {
 				consumer.accept(s, t);
@@ -113,7 +119,10 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <S, T> BiConsumer<S, T> checkSecondThenMaybeConsume(@NotNull Predicate<T> predicate, @NotNull BiConsumer<S, T> consumer) {
+	static <S, T> BiConsumer<S, T> checkSecondThenMaybeConsume(
+			@NotNull Predicate<T> predicate,
+			@NotNull BiConsumer<S, T> consumer
+	) {
 		return (s, t) -> {
 			if (predicate.test(t)) {
 				consumer.accept(s, t);
@@ -122,52 +131,58 @@ public class FunctionUtils {
 	}
 
 	@NotNull
-	public static <R, S, T> BiConsumer<R, T> mapFirstThenConsume(@NotNull Function<R, S> function, @NotNull BiConsumer<S, T> consumer) {
+	static <R, S, T> BiConsumer<R, T> mapFirstThenConsume(
+			@NotNull Function<R, S> function,
+			@NotNull BiConsumer<S, T> consumer
+	) {
 		return (r, t) -> consumer.accept(function.apply(r), t);
 	}
 
 	@NotNull
-	public static <R, S, T> BiConsumer<S, R> mapSecondThenConsume(@NotNull Function<R, T> function, @NotNull BiConsumer<S, T> consumer) {
+	static <R, S, T> BiConsumer<S, R> mapSecondThenConsume(
+			@NotNull Function<R, T> function,
+			@NotNull BiConsumer<S, T> consumer
+	) {
 		return (s, r) -> consumer.accept(s, function.apply(r));
 	}
 
 	@NotNull
-	public <R, S, T> Function<R, Function<S, T>> curry(@NotNull BiFunction<R, S, T> function) {
+	static <R, S, T> Function<R, Function<S, T>> curry(@NotNull BiFunction<R, S, T> function) {
 		return r -> s -> function.apply(r, s);
 	}
 
 	@NotNull
-	public <R, S> Function<R, Consumer<S>> curry(@NotNull BiConsumer<R, S> consumer) {
+	static <R, S> Function<R, Consumer<S>> curry(@NotNull BiConsumer<R, S> consumer) {
 		return r -> s -> consumer.accept(r, s);
 	}
 
 	@NotNull
-	public <Q, R, S, T> Function<Q, Function<R, Function<S, T>>> curry(@NotNull TriFunction<Q, R, S, T> function) {
+	static <Q, R, S, T> Function<Q, Function<R, Function<S, T>>> curry(@NotNull TriFunction<Q, R, S, T> function) {
 		return q -> r -> s -> function.apply(q, r, s);
 	}
 
 	@NotNull
-	public <Q, R, S> Function<Q, Function<R, Consumer<S>>> curry(@NotNull TriConsumer<Q, R, S> consumer) {
+	static <Q, R, S> Function<Q, Function<R, Consumer<S>>> curry(@NotNull TriConsumer<Q, R, S> consumer) {
 		return q -> r -> s -> consumer.accept(q, r, s);
 	}
 
 	@NotNull
-	public <R, S, T, E extends Exception> Function<R, ExceptionalFunction<S, T, E>> curry(@NotNull ExceptionalBiFunction<R, S, T, E> function) {
+	static <R, S, T, E extends Exception> Function<R, ExceptionalFunction<S, T, E>> curry(@NotNull ExceptionalBiFunction<R, S, T, E> function) {
 		return r -> s -> function.apply(r, s);
 	}
 
 	@NotNull
-	public <R, S, E extends Exception> Function<R, ExceptionalConsumer<S, E>> curry(@NotNull ExceptionalBiConsumer<R, S, E> consumer) {
+	static <R, S, E extends Exception> Function<R, ExceptionalConsumer<S, E>> curry(@NotNull ExceptionalBiConsumer<R, S, E> consumer) {
 		return r -> s -> consumer.accept(r, s);
 	}
 
 	@NotNull
-	public <Q, R, S, T, E extends Exception> Function<Q, Function<R, ExceptionalFunction<S, T, E>>> curry(@NotNull ExceptionalTriFunction<Q, R, S, T, E> function) {
+	static <Q, R, S, T, E extends Exception> Function<Q, Function<R, ExceptionalFunction<S, T, E>>> curry(@NotNull ExceptionalTriFunction<Q, R, S, T, E> function) {
 		return q -> r -> s -> function.apply(q, r, s);
 	}
 
 	@NotNull
-	public <Q, R, S, E extends Exception> Function<Q, Function<R, ExceptionalConsumer<S, E>>> curry(@NotNull ExceptionalTriConsumer<Q, R, S, E> consumer) {
+	static <Q, R, S, E extends Exception> Function<Q, Function<R, ExceptionalConsumer<S, E>>> curry(@NotNull ExceptionalTriConsumer<Q, R, S, E> consumer) {
 		return q -> r -> s -> consumer.accept(q, r, s);
 	}
 }
