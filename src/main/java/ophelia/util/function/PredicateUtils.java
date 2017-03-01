4357ca4aec8bba6a12d646177923e29a08f01ffa
Steven Weston <echogene.alpha@gmail.com>
2017-03-01 20:41:56 +0000

    Add some more predicates

--- src/main/java/ophelia/util/function/PredicateUtils.java
+++ src/main/java/ophelia/util/function/PredicateUtils.java
@@ -2,6 +2,7 @@ package ophelia.util.function;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 import java.util.function.Function;
@@ -33,5 +34,25 @@ public interface PredicateUtils {
 	@NotNull
 	static <S, T> Predicate<? super T> isNull(@NotNull Function<? super T, ? extends S> function) {
 		return t -> Objects.isNull(function.apply(t));
+	}
+
+	@NotNull
+	@Contract(pure = true)
+	static <T> Predicate<? super T> equals(@Nullable T equalTo) {
+		if (equalTo == null) {
+			return isNull();
+		} else {
+			return equalTo::equals;
+		}
+	}
+
+	@NotNull
+	@Contract(pure = true)
+	static <T> Predicate<? super T> notEquals(@Nullable T equalTo) {
+		if (equalTo == null) {
+			return notNull();
+		} else {
+			return t -> !equalTo.equals(t);
+		}
 	}
 }
