76f370e887b3d03b51d75a1f2937a707765b79f0
Steven Weston <echogene.alpha@gmail.com>
2017-03-01 21:05:06 +0000

    Add default methods

--- src/main/java/ophelia/function/ExceptionalConsumer.java
+++ src/main/java/ophelia/function/ExceptionalConsumer.java
@@ -1,11 +1,53 @@
 package ophelia.function;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
 /**
- * Like {@link java.util.function.Consumer} but can throw an exception.
+ * Like {@link Consumer} but can throw an exception.
  * @author Steven Weston
  */
 @FunctionalInterface
 public interface ExceptionalConsumer<T, E extends Exception> {
 
 	void accept(T t) throws E;
+
+	@NotNull
+	default ExceptionalConsumer<T, E> andThen(@NotNull Consumer<? super T> after) {
+		return t -> {
+			accept(t);
+			after.accept(t);
+		};
+	}
+
+	@NotNull
+	default ExceptionalConsumer<T, E> andThen(@NotNull ExceptionalConsumer<? super T, ? extends E> after) {
+		return t -> {
+			accept(t);
+			after.accept(t);
+		};
+	}
+
+	@NotNull
+	default ExceptionalConsumer<T, E> andFinally(@NotNull Consumer<? super T> after) {
+		return t -> {
+			try {
+				accept(t);
+			} finally {
+				after.accept(t);
+			}
+		};
+	}
+
+	@NotNull
+	default ExceptionalConsumer<T, E> andFinally(@NotNull ExceptionalConsumer<? super T, ? extends E> after) {
+		return t -> {
+			try {
+				accept(t);
+			} finally {
+				after.accept(t);
+			}
+		};
+	}
 }
