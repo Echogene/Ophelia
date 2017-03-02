65721093afc7e969f6f6b3bc2db45ca14b034714
Steven Weston <echogene.alpha@gmail.com>
2017-03-02 18:57:24 +0000

    Add some default methods

--- src/main/java/ophelia/function/ExceptionalTriFunction.java
+++ src/main/java/ophelia/function/ExceptionalTriFunction.java
@@ -1,6 +1,9 @@
 package ophelia.function;
 
 import com.codepoetics.protonpack.functions.TriFunction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 /**
  * A functional interface that accepts three objects and returns one but can throw an exception.
@@ -11,4 +14,14 @@ import com.codepoetics.protonpack.functions.TriFunction;
 public interface ExceptionalTriFunction<A, B, C, R, E extends Exception> {
 
 	R apply(A a, B b, C c) throws E;
+
+	@NotNull
+	default <V> ExceptionalTriFunction<A, B, C, V, E> andThen(@NotNull Function<? super R, ? extends V> after) {
+		return (a, b, c) -> after.apply(apply(a, b, c));
+	}
+
+	@NotNull
+	default <V> ExceptionalTriFunction<A, B, C, V, E> andThen(@NotNull ExceptionalFunction<? super R, ? extends V, ? extends E> after) {
+		return (a, b, c) -> after.apply(apply(a, b, c));
+	}
 }
