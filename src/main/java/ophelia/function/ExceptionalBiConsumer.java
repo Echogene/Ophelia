a6395872e17aaa33c033bd2e4004c68ed889f790
Steven Weston <echogene.alpha@gmail.com>
2017-03-02 18:51:12 +0000

    Add some default methods

--- src/main/java/ophelia/function/ExceptionalBiConsumer.java
+++ src/main/java/ophelia/function/ExceptionalBiConsumer.java
@@ -1,11 +1,53 @@
 package ophelia.function;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.BiConsumer;
+
 /**
- * Like {@link java.util.function.BiConsumer} but can throw an exception.
+ * Like {@link BiConsumer} but can throw an exception.
  * @author Steven Weston
  */
 @FunctionalInterface
 public interface ExceptionalBiConsumer<T, U, E extends Exception> {
 
 	void accept(T t, U u) throws E;
+
+	@NotNull
+	default ExceptionalBiConsumer<T, U, E> andThen(@NotNull BiConsumer<? super T, ? super U> after) {
+		return (t, u) -> {
+			accept(t, u);
+			after.accept(t, u);
+		};
+	}
+
+	@NotNull
+	default ExceptionalBiConsumer<T, U, E> andThen(@NotNull ExceptionalBiConsumer<? super T, ? super U, ? extends E> after) {
+		return (t, u) -> {
+			accept(t, u);
+			after.accept(t, u);
+		};
+	}
+
+	@NotNull
+	default ExceptionalBiConsumer<T, U, E> andFinally(@NotNull BiConsumer<? super T, ? super U> after) {
+		return (t, u) -> {
+			try {
+				accept(t, u);
+			} finally {
+				after.accept(t, u);
+			}
+		};
+	}
+
+	@NotNull
+	default ExceptionalBiConsumer<T, U, E> andFinally(@NotNull ExceptionalBiConsumer<? super T, ? super U, ? extends E> after) {
+		return (t, u) -> {
+			try {
+				accept(t, u);
+			} finally {
+				after.accept(t, u);
+			}
+		};
+	}
 }
