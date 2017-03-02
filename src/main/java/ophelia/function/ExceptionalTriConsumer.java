59afab37ba16515ca3f0b3e4b1e53edf7e334ad9
Steven Weston <echogene.alpha@gmail.com>
2017-03-02 18:54:23 +0000

    Add some default methods

--- src/main/java/ophelia/function/ExceptionalTriConsumer.java
+++ src/main/java/ophelia/function/ExceptionalTriConsumer.java
@@ -1,5 +1,7 @@
 package ophelia.function;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * A functional interface that accepts three objects and can throw an exception.
  * @see TriConsumer
@@ -9,4 +11,42 @@ package ophelia.function;
 public interface ExceptionalTriConsumer<T, U, V, E extends Exception> {
 
 	void accept(T t, U u, V v) throws E;
+
+	@NotNull
+	default ExceptionalTriConsumer<T, U, V, E> andThen(@NotNull TriConsumer<? super T, ? super U, ? super V> after) {
+		return (t, u, v) -> {
+			accept(t, u, v);
+			after.accept(t, u, v);
+		};
+	}
+
+	@NotNull
+	default ExceptionalTriConsumer<T, U, V, E> andThen(@NotNull ExceptionalTriConsumer<? super T, ? super U, ? super V, ? extends E> after) {
+		return (t, u, v) -> {
+			accept(t, u, v);
+			after.accept(t, u, v);
+		};
+	}
+
+	@NotNull
+	default ExceptionalTriConsumer<T, U, V, E> andFinally(@NotNull TriConsumer<? super T, ? super U, ? super V> after) {
+		return (t, u, v) -> {
+			try {
+				accept(t, u, v);
+			} finally {
+				after.accept(t, u, v);
+			}
+		};
+	}
+
+	@NotNull
+	default ExceptionalTriConsumer<T, U, V, E> andFinally(@NotNull ExceptionalTriConsumer<? super T, ? super U, ? super V, ? extends E> after) {
+		return (t, u, v) -> {
+			try {
+				accept(t, u, v);
+			} finally {
+				after.accept(t, u, v);
+			}
+		};
+	}
 }
