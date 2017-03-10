871ee3c444b3037156c71a54fd7f71c54951b815
Steven Weston <echogene.alpha@gmail.com>
2017-03-10 21:12:07 +0000

    Add hierarchy parser

---
+++ src/main/java/ophelia/util/hierarchy/HierarchyParser.java
@@ -0,0 +1,135 @@
+package ophelia.util.hierarchy;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class HierarchyParser<T, C extends Collection<T>> {
+
+	@NotNull
+	private final Indentation indentation;
+
+	@NotNull
+	private final Function<String, T> nodeConstructor;
+
+	@NotNull
+	private final BiConsumer<T, C> childrenSetter;
+
+	@NotNull
+	private final Supplier<C> childrenConstructor;
+
+	private HierarchyParser(
+			@NotNull Indentation indentation,
+			@NotNull Function<String, T> nodeConstructor,
+			@NotNull BiConsumer<T, C> childrenSetter,
+			@NotNull Supplier<C> childrenConstructor
+	) {
+		this.indentation = indentation;
+		this.nodeConstructor = nodeConstructor;
+		this.childrenSetter = (t, c) -> {
+			if (t != null) {
+				childrenSetter.accept(t, c);
+			}
+		};
+		this.childrenConstructor = childrenConstructor;
+	}
+
+	@NotNull
+	public static <T> HierarchyParser<T, List<T>> forChildList(
+			@NotNull Function<String, T> nodeConstructor,
+			@NotNull BiConsumer<T, List<T>> childrenSetter
+	) {
+		return new HierarchyParser<>(
+				Indentation.ofString(" "),
+				nodeConstructor,
+				childrenSetter,
+				ArrayList::new
+		);
+	}
+
+	/**
+	 * Given a string representation of a hierarchy, return a list of its root nodes, which should be populated with
+	 * children.
+	 * @param rows the rows that represent a hierarchy where children are indented using the given {@link #indentation}
+	 * @return the list of root nodes
+	 */
+	@NotNull
+	public C parse(@NotNull String... rows) {
+		HierarchyState state = new HierarchyState(rows);
+		return state.process();
+	}
+
+	private class HierarchyState {
+		@Nullable
+		T lastNode = globalNode();
+
+		@NotNull
+		final Map<T, C> parentToChildren = new HashMap<T, C>() {{
+			// Make sure the global node has a an empty list of children, rather than a null one.
+			put(globalNode(), childrenConstructor.get());
+		}};
+
+		int lastIndentation = -1;
+
+		@NotNull
+		final Stack<T> currentAncestors = new Stack<>();
+
+		@NotNull
+		final String[] rows;
+
+		HierarchyState(@NotNull String[] rows) {
+			this.rows = rows;
+		}
+
+		C process() {
+			for (String row : rows) {
+				process(row);
+			}
+			parentToChildren.forEach(childrenSetter);
+			return parentToChildren.get(globalNode());
+		}
+
+		void process(@NotNull String currentRow) {
+
+			int currentIndentation = indentation.getLevelOfIndentation(currentRow);
+			T currentNode = nodeConstructor.apply(indentation.getUnindentedString(currentRow));
+
+			if (currentIndentation >= lastIndentation) {
+				if (currentIndentation - lastIndentation > 1) {
+					throw new RuntimeException("A child node cannot be indented more than once after its parent.");
+
+				} else if (currentIndentation - lastIndentation == 1) {
+					// We've gone down another level, so the last node is the new parent.
+					currentAncestors.push(lastNode);
+					parentToChildren.put(lastNode, childrenConstructor.get());
+				}
+			} else {
+				// We've gone back up at least one level, so we need to update the current parent.
+				int goBackUp = lastIndentation - currentIndentation;
+				for (int i = 0; i < goBackUp; i++) {
+					currentAncestors.pop();
+				}
+			}
+			C siblings = parentToChildren.get(currentAncestors.peek());
+			if (siblings == null) {
+				throw new RuntimeException("The siblings should not be null.  Does the provided "
+						+ "childrenConstructor return null?");
+			}
+			siblings.add(currentNode);
+
+			lastNode = currentNode;
+			lastIndentation = currentIndentation;
+		}
+	}
+
+	@Nullable
+	@Contract(value = "-> null", pure = true)
+	private T globalNode() {
+		return null;
+	}
+}
---
+++ src/test/java/ophelia/util/HierarchyParserTest.java
@@ -0,0 +1,258 @@
+package ophelia.util;
+
+import ophelia.util.hierarchy.HierarchyParser;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static ophelia.util.hierarchy.TreeUtils.prettyPrint;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+
+public class HierarchyParserTest {
+
+	@Test
+	public void should_parse_empty_hierarchy() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse();
+
+		assertThat(hierarchy, is(empty()));
+	}
+
+	@Test
+	public void should_parse_one_root_node() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse("lol");
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_two_root_nodes() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						"rofl"
+				);
+
+		assertThat(hierarchy, hasSize(2));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, is(empty()));
+
+		Foo rofl = hierarchy.get(1);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_a_root_node_and_its_child() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl"
+				);
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, hasSize(1));
+
+		Foo rofl = lol.children.get(0);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_a_root_node_and_its_two_children() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl",
+						" garply"
+				);
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, hasSize(2));
+
+		Foo rofl = lol.children.get(0);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, is(empty()));
+
+		Foo garply = lol.children.get(1);
+		assertThat(garply.bar, is("garply"));
+		assertThat(garply.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_a_root_node_child_and_grandchild() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl",
+						"  garply"
+				);
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, hasSize(1));
+
+		Foo rofl = lol.children.get(0);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, hasSize(1));
+
+		Foo garply = rofl.children.get(0);
+		assertThat(garply.bar, is("garply"));
+		assertThat(garply.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_two_root_nodes_where_the_second_one_has_a_child() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						"rofl",
+						" garply"
+				);
+
+		assertThat(hierarchy, hasSize(2));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, is(empty()));
+
+		Foo rofl = hierarchy.get(1);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, hasSize(1));
+
+		Foo garply = rofl.children.get(0);
+		assertThat(garply.bar, is("garply"));
+		assertThat(garply.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_a_root_node_with_two_children_where_the_first_itself_has_a_child() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl",
+						"  garply",
+						" qux"
+				);
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, hasSize(2));
+
+		Foo rofl = lol.children.get(0);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, hasSize(1));
+
+		Foo garply = rofl.children.get(0);
+		assertThat(garply.bar, is("garply"));
+		assertThat(garply.children, is(empty()));
+
+		Foo qux = lol.children.get(1);
+		assertThat(qux.bar, is("qux"));
+		assertThat(qux.children, is(empty()));
+	}
+
+	@Test
+	public void should_parse_a_root_node_with_two_children_where_the_second_itself_has_a_child() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl",
+						" garply",
+						"  qux"
+				);
+
+		assertThat(hierarchy, hasSize(1));
+
+		Foo lol = hierarchy.get(0);
+		assertThat(lol.bar, is("lol"));
+		assertThat(lol.children, hasSize(2));
+
+		Foo rofl = lol.children.get(0);
+		assertThat(rofl.bar, is("rofl"));
+		assertThat(rofl.children, is(empty()));
+
+		Foo garply = lol.children.get(1);
+		assertThat(garply.bar, is("garply"));
+		assertThat(garply.children, hasSize(1));
+
+		Foo qux = garply.children.get(0);
+		assertThat(qux.bar, is("qux"));
+		assertThat(qux.children, is(empty()));
+	}
+
+	@Test
+	public void complex_hierarchy_should_look_alright() throws Exception {
+		List<Foo> hierarchy = HierarchyParser.forChildList(Foo::new, Foo::setChildren)
+				.parse(
+						"lol",
+						" rofl",
+						"  wibble",
+						"   wobble",
+						"    wubble",
+						" garply",
+						"  qux",
+						" gorply",
+						"  qix",
+						"   qax",
+						"   frog",
+						"fred",
+						" frobble"
+				);
+
+		String pretty = prettyPrint(hierarchy, foo -> foo.bar, foo -> foo.children, 1);
+		assertThat(pretty, is (
+				"lol\n" +
+				"├─rofl\n" +
+				"│ └─wibble\n" +
+				"│   └─wobble\n" +
+				"│     └─wubble\n" +
+				"├─garply\n" +
+				"│ └─qux\n" +
+				"└─gorply\n" +
+				"  └─qix\n" +
+				"    ├─qax\n" +
+				"    └─frog\n" +
+				"fred\n" +
+				"└─frobble\n"
+		));
+	}
+
+	class Foo {
+		@NotNull
+		final String bar;
+
+		@NotNull
+		List<Foo> children = emptyList();
+
+		Foo(@NotNull String bar) {
+			this.bar = bar;
+		}
+
+		void setChildren(@NotNull List<Foo> children) {
+			this.children = children;
+		}
+	}
+}
