efabaca9b0a6240a989e8307059600142dcef71e
Steven Weston <echogene.alpha@gmail.com>
2017-04-13 17:46:39 +0100

    Strengthen tests

--- src/test/java/ophelia/util/CurrentIteratorTest.java
+++ src/test/java/ophelia/util/CurrentIteratorTest.java
@@ -2,13 +2,13 @@ package ophelia.util;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static ophelia.util.function.FunctionUtils.ignoreExceptions;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Steven Weston
@@ -20,7 +20,9 @@ public class CurrentIteratorTest {
 
 		CurrentIterator<Object> iterator = new CurrentIterator<>(Collections.emptySet().iterator());
 
+		assertFalse(iterator.hasNext());
 		assertNull(iterator.current());
+		assertFalse(iterator.hasNext());
 	}
 
 	@Test(expected = NoSuchElementException.class)
@@ -30,6 +32,7 @@ public class CurrentIteratorTest {
 
 		ignoreExceptions(iterator, Iterator::next);
 
+		assertFalse(iterator.hasNext());
 		iterator.current();
 	}
 
@@ -39,9 +42,41 @@ public class CurrentIteratorTest {
 		String lol = "lol";
 		CurrentIterator<String> iterator = new CurrentIterator<>(Collections.singleton(lol).iterator());
 
+		assertTrue(iterator.hasNext());
 		assertNull(iterator.current());
+		assertTrue(iterator.hasNext());
+
 		assertEquals(lol, iterator.next());
+
+		assertFalse(iterator.hasNext());
 		assertEquals(lol, iterator.current());
 		assertEquals(lol, iterator.current());
+		assertFalse(iterator.hasNext());
+	}
+
+	@Test
+	public void test_on_set() throws Exception {
+
+		String lol = "lol";
+		String rofl = "rofl";
+		CurrentIterator<String> iterator = new CurrentIterator<>(Arrays.asList(lol, rofl).iterator());
+
+		assertTrue(iterator.hasNext());
+		assertNull(iterator.current());
+		assertTrue(iterator.hasNext());
+
+		assertEquals(lol, iterator.next());
+
+		assertTrue(iterator.hasNext());
+		assertEquals(lol, iterator.current());
+		assertEquals(lol, iterator.current());
+		assertTrue(iterator.hasNext());
+
+		assertEquals(rofl, iterator.next());
+
+		assertFalse(iterator.hasNext());
+		assertEquals(rofl, iterator.current());
+		assertEquals(rofl, iterator.current());
+		assertFalse(iterator.hasNext());
 	}
 }
